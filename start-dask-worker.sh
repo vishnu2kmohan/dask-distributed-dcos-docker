e94f9a653007572aa9bd95bca880e4b00e417dfd
Vishnu Mohan <vishnu@mesosphere.com>
2016-10-24 14:40:00 -0400

    Fixup worker startup script and Marathon Group JSON

--- start-dask-worker.sh
+++ start-dask-worker.sh
@@ -9,9 +9,8 @@ if [ \( -n "${MARATHON_APP_ID-}" \) -a \( -n "${MARATHON_APP_RESOURCE_CPUS-}" \)
     -a \( -n "${MESOS_TASK_ID-}" \) -a \( -n "${LIBPROCESS_IP-}" \) \
     -a \( -n "${PORT1-}" \) -a \( -n "${PORT2-}" \) -a \( -n "${PORT3-}" \) ]
 then
-    SCHEDULER_VIP_PREFIX=$(python -c \
+    VIP_PREFIX=$(python -c \
         "import os; print(''.join(os.environ['MARATHON_APP_ID'].split('/')[:-1]))")
-    DASK_SCHEDULER="${SCHEDULER_VIP_PREFIX}dask-scheduler:8786"
     dask-worker \
     --worker-port "${PORT1}" \
     --http-port "${PORT2}" \
@@ -21,7 +20,7 @@ then
     --nprocs $(python -c \
         "import os,math; print(math.ceil(os.environ['MARATHON_APP_RESOURCE_CPUS'])") \
     --name "${MESOS_TASK_ID}" \
-    "${DASK_SCHEDULER}"
+        "${VIP_PREFIX}dask-scheduler.marathon.l4lb.thisdcos.directory:8786"
 else
     dask-worker "$@"
 fi
