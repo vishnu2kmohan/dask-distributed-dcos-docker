f3f2bb2935654054d32e5ed65a325ff08ba549f9
Vishnu Mohan <vishnu@mesosphere.com>
2016-10-24 18:12:56 -0400

    Add support for --memory-limit based on 80% of MARATHON_APP_RESOURCE_MEM

--- start-dask-worker.sh
+++ start-dask-worker.sh
@@ -26,6 +26,12 @@ then
         FW_NAME="marathon"
     fi
 
+    # Set Dask Worker Memory Limit to be 80% of ${MARATHON_APP_RESOURCE_MEM}
+    NBYTES=$(python -c \
+        "import os; print('',join([str(int(float(os.environ['MARATHON_APP_RESOURCE_MEM']) * 0.8)), 'e6']))" \
+    )
+    echo "Memory Limit in Bytes (1 Megabyte=1e6, 1 Gigabyte=1e9): ${NBYTES}"
+
     DASK_SCHEDULER="${VIP_PREFIX}dask-scheduler.${FW_NAME}.l4lb.thisdcos.directory:8786"
     echo "Dask Scheduler: ${DASK_SCHEDULER}"
 
@@ -36,6 +42,7 @@ then
         --host "${LIBPROCESS_IP}" \
         --nthreads 1 \
         --nprocs "${NPROCS}" \
+        --memory-limit "${NBYTES}" \
         --name "${MESOS_TASK_ID}" \
         "${DASK_SCHEDULER}"
 else
