7bb123860c146ca182377f45fc6e44b238d9d900
Vishnu Mohan <vishnu@mesosphere.com>
2017-03-20 15:19:12 -0400

    Align Scheduler Bokeh App Prefix with HAProxy directive

--- start-dask-scheduler.sh
+++ start-dask-scheduler.sh
@@ -5,15 +5,18 @@ set -o errexit -o pipefail
 source "$HOME/.bash_profile"
 source activate dask-distributed
 
-if [ \( -n "${MARATHON_APP_ID-}" \) -a \( -n "${MARATHON_APP_RESOURCE_CPUS-}" \) \
-    -a \( -n "${MESOS_TASK_ID-}" \) -a \( -n "${HOST-}" \) \
+if [ \( -n "${MARATHON_APP_ID-}" \) -a \( -n "${HOST-}" \) \
     -a \( -n "${PORT1-}" \) -a \( -n "${PORT2-}" \) \
     -a \( -n "${PORT3-}" \) -a \( -n "${PORT4-}" \) ]
 then
-    SCHEDULER_APP_PREFIX=$(python -c \
-        "import os; print('-'.join(os.environ['MARATHON_APP_ID'].split('/')[1:-1]))" \
-    )
-    echo "Dask Scheduler App Prefix: ${SCHEDULER_APP_PREFIX}"
+    SCHEDULER_BOKEH_APP_PREFIX=""
+
+    if [ -n "${MARATHON_APP_LABEL_HAPROXY_3_PATH-}" ]
+    then
+        SCHEDULER_BOKEH_APP_PREFIX="${MARATHON_APP_LABEL_HAPROXY_3_PATH}"
+    fi
+    
+    echo "Dask Scheduler Bokeh App Prefix: ${SCHEDULER_BOKEH_APP_PREFIX}"
 
     dask-scheduler \
         --host "*" \
@@ -21,7 +24,7 @@ then
         --http-port "${PORT2}" \
         --bokeh-port "${PORT3}" \
         --bokeh-internal-port "${PORT4}" \
-        --prefix "${SCHEDULER_APP_PREFIX}"
+        --prefix "${SCHEDULER_BOKEH_APP_PREFIX}"
 else
     dask-scheduler "$@"
 fi
