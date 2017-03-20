a948bd9f7f256d00014185664d0713abc1215170
Vishnu Mohan <vishnu@mesosphere.com>
2017-03-20 16:13:31 -0400

    Reintroduce X-Headers support

--- start-dask-scheduler.sh
+++ start-dask-scheduler.sh
@@ -11,6 +11,7 @@ if [ \( -n "${MARATHON_APP_ID-}" \) -a \( -n "${HOST-}" \) \
 then
     dask-scheduler \
         --host "*" \
+        --use-xheaders "True" \
         --port "${PORT1}" \
         --http-port "${PORT2}" \
         --bokeh-port "${PORT3}" \
