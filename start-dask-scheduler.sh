5390efa745ef5b6a26a7e91de8697d58afe4e1d4
Vishnu Mohan <vishnu@mesosphere.com>
2017-03-20 16:01:38 -0400

    Get rid of the Schedler Bokeh App Prefix

--- start-dask-scheduler.sh
+++ start-dask-scheduler.sh
@@ -9,22 +9,12 @@ if [ \( -n "${MARATHON_APP_ID-}" \) -a \( -n "${HOST-}" \) \
     -a \( -n "${PORT1-}" \) -a \( -n "${PORT2-}" \) \
     -a \( -n "${PORT3-}" \) -a \( -n "${PORT4-}" \) ]
 then
-    SCHEDULER_BOKEH_APP_PREFIX=""
-
-    if [ -n "${MARATHON_APP_LABEL_HAPROXY_3_PATH-}" ]
-    then
-        SCHEDULER_BOKEH_APP_PREFIX="${MARATHON_APP_LABEL_HAPROXY_3_PATH}"
-    fi
-    
-    echo "Dask Scheduler Bokeh App Prefix: ${SCHEDULER_BOKEH_APP_PREFIX}"
-
     dask-scheduler \
         --host "*" \
         --port "${PORT1}" \
         --http-port "${PORT2}" \
         --bokeh-port "${PORT3}" \
-        --bokeh-internal-port "${PORT4}" \
-        --prefix "${SCHEDULER_BOKEH_APP_PREFIX}"
+        --bokeh-internal-port "${PORT4}"
 else
     dask-scheduler "$@"
 fi
