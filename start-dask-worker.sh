bd8c0f1d2f1b248c7f73e46ed08fca756d9b7cf6
Vishnu Mohan <vishnu@mesosphere.com>
2017-03-26 11:12:40 -0400

    Set Dask Resources to enable sophisticated task placement on workers

--- start-dask-worker.sh
+++ start-dask-worker.sh
@@ -8,6 +8,7 @@ source activate dask-distributed
 if [ \( -n "${MARATHON_APP_ID-}" \) \
     -a \( -n "${MARATHON_APP_RESOURCE_CPUS-}" \) \
     -a \( -n "${MARATHON_APP_RESOURCE_MEM-}" \) \
+    -a \( -n "${MARATHON_APP_RESOURCE_DISK-}" \) \
     -a \( -n "${MESOS_TASK_ID-}" \) -a \( -n "${HOST-}" \) \
     -a \( -n "${PORT1-}" \) -a \( -n "${PORT2-}" \) \
     -a \( -n "${PORT3-}" \) -a \( -n "${PORT4-}" \) ]
@@ -38,6 +39,17 @@ then
     DASK_SCHEDULER="${VIP_PREFIX}.dask-scheduler.${FW_NAME}.l4lb.thisdcos.directory:8786"
     echo "Dask Scheduler: ${DASK_SCHEDULER}"
 
+    # Set Dask Resources to enable sophisticated task placement on workers
+    # https://distributed.readthedocs.io/en/latest/resources.html
+    DASK_RESOURCES="CPUS=${MARATHON_APP_RESOURCE_CPUS}"
+    DASK_RESOURCES="${DASK_RESOURCES} MEM=${MARATHON_APP_RESOURCE_MEM}"
+    DASK_RESOURCES="${DASK_RESOURCES} DISK=${MARATHON_APP_RESOURCE_DISK}"
+    if [ -n "${MARATHON_APP_RESOURCE_GPUS-}" ]
+    then
+        DASK_RESOURCES="${DASK_RESOURCES} GPUS=${MARATHON_APP_RESOURCE_GPUS}"
+    fi
+    echo "Dask Resources: ${DASK_RESOURCES}"
+
     dask-worker \
         --host "${HOST}" \
         --worker-port "${PORT1}" \
@@ -49,6 +61,7 @@ then
         --memory-limit "${NBYTES}" \
         --name "${MESOS_TASK_ID}" \
         --local-directory "${MESOS_SANDBOX}" \
+        --resources "${DASK_RESOURCES}" \
         "${DASK_SCHEDULER}"
 else
     dask-worker "$@"
